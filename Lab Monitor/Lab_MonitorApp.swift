74a6b3f10fb7476ff237f208cfe8a440f32bceef
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-01-31 13:33:16 +0000

    Initial Commit

---
+++ Lab Monitor/Lab_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab_MonitorApp.swift
+//  Lab Monitor
+//
+//  Created by James on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
