d7efb0492742e0c54ceb3ffac94df79b7c8d3757
James Harvey <44349936+jmshrv@users.noreply.github.com>
2024-02-04 00:20:58 +0000

    Fix Sundays showing the wrong week

--- Lab Monitor/Extensions/Calendar+daysOfWeek.swift
+++ Lab Monitor/Extensions/Calendar+daysOfWeek.swift
@@ -10,7 +10,14 @@ import Foundation
 extension Calendar {
     var daysOfWeek: [Date] {
         let today = startOfDay(for: Date())
-        let dayOfWeek = component(.weekday, from: today) - 1
+        var dayOfWeek = component(.weekday, from: today) - 1
+        
+//        Awkwardly work around the whole "weekdays start on Sunday" thing
+//        Surely there's a way to start the week on Monday in Swift???
+        if dayOfWeek == 0 {
+            dayOfWeek = 7
+        }
+        
         return range(of: .weekday, in: .weekOfYear, for: today)!
             .compactMap { date(byAdding: .day, value: $0 - dayOfWeek, to: today) }
     }
